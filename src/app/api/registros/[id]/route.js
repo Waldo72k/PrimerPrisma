5b166867e51179cce425513a758fa86f1a23e78c
Waldo <oswaldo.ct18@gmail.com>
2023-10-19 19:54:55 -0700

    Agregue funcion de PUT y DELETE

--- src/app/api/registros/[id]/route.js
+++ src/app/api/registros/[id]/route.js
@@ -23,14 +23,72 @@ export async function GET(request,{params: {id}}) {
     }
 }
 
-export async function DELETE(request) {
-    return NextResponse.json({
-        mensaje: 'Eliminando un estudiante'
-    })
-}
+export async function DELETE(request, { params: { id } }) {
+    try {
+      const existingStudent = await prisma.estudiante.findFirst({
+        where: {
+          id: Number(id),
+        },
+      });
+  
+      if (!existingStudent) {
+        return NextResponse.json(
+          { mensaje: "El estudiante no existe" },
+          { status: 404 }
+        );
+      }
+  
+      await prisma.estudiante.delete({
+        where: {
+          id: Number(id),
+        },
+      });
+  
+      return NextResponse.json(
+        { mensaje: "Estudiante eliminado con éxito" },
+        { status: 200 }
+      );
+    } catch (error) {
+      if (error instanceof Error) {
+        return NextResponse.json(error.message, { status: 500 });
+      }
+    }
+  }
+  
 
-export async function PUT(request) {
-    return NextResponse.json({
-        mensaje: 'Actualizando un estudiante'
-    })
-}
+  export async function PUT(request, { params: { id }}) {
+    try {
+      const existingStudent = await prisma.estudiante.findFirst({
+        where: {
+          id: Number(id),
+        },
+      });
+  
+      if (!existingStudent) {
+        return NextResponse.json(
+          { mensaje: "El estudiante no existe" },
+          { status: 404 }
+        );
+      }
+  
+      const {nombre,genero,edad,carrera} = await request.json()
+        const actualizarEstudiante = await prisma.estudiante.update({
+            where: {
+                id: Number(id),
+              },
+            data:{
+                nombre,
+                genero,
+                edad,
+                carrera
+            }
+        })
+  
+      return NextResponse.json(actualizarEstudiante, { status: 200 });
+    } catch (error) {
+      if (error instanceof Error) {
+        return NextResponse.json(error.message, { status: 500 });
+      }
+    }
+  }
+  
